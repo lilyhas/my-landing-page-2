34e227031df03c29f3fd826dcac301e143115b0c
lilyhas <115729520+lilyhas@users.noreply.github.com>
2024-04-14 23:36:09 +0100

    Add files via upload

---
+++ javascript/sketch.js
@@ -0,0 +1,15 @@
+/*
+
+Lili Chasioti
+WCC_W2 - Tell me a Joke 
+
+A sketch that uses an open API for jokes, to display jokes on the press of a button. 
+Understanding DOM elements and integrating APIs
+
+*/
+
+$('body').ripples({
+  resolution: 512,
+  dropRadius: 20,
+  perturbance: 0.04,
+});
